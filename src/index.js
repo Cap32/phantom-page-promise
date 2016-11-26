d342ba3bad387c2ef552195cf37b47ad27f15c08
Webb Peng <webb.png@gmail.com>
2016-11-26 15:47:59 +0800

    add error handler

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 
 import Page from 'phantom/lib/page';
 
-const Consts = {
-	POLYFILL: 'node_modules/promise-polyfill/promise.min.js',
-	CALL_PHANTOM: '_evaluatePromise',
-	HAS_INJECTED: '_hasInjectedPromise',
-	TIMEOUT_ERROR: 'TIMEOUT',
-	ERROR_PREFIX: 'Run `evaluatePromise` error',
-	DEFAULT_TIMEOUT: 10000,
-};
+const PROMISE_POLYFILL = require.resolve('promise-polyfill/promise.min.js');
+const CALL_PHANTOM_FULFILLED = '_evaluatePromiseFulfilled';
+const CALL_PHANTOM_REJECTED = '_evaluatePromiseRejected';
+const HAS_INJECTED = '_hasInjectedPromise';
+const TIMEOUT_ERROR = 'TIMEOUT';
+const ERROR_PREFIX = 'Run `evaluatePromise` error';
+const DEFAULT_TIMEOUT = 10000;
 
 const execDelay = (t = 2000) => new Promise((done) => setTimeout(done, t));
 
 Page.prototype.evaluatePromise = async function (src, options = {}) {
 	const {
-		timeout = Consts.DEFAULT_TIMEOUT,
+		timeout = DEFAULT_TIMEOUT,
 		delay,
 		args = [],
 	} = options;
 	const code = typeof src === 'string' ? src : src.toString();
 
 	const execTimeout = new Promise((r, reject) => {
-		setTimeout(() => reject(Consts.TIMEOUT_ERROR), timeout);
+		setTimeout(() => reject(TIMEOUT_ERROR), timeout);
 	});
 
-	if (!await this.property(Consts.HAS_INJECTED)) {
-		const injected = await this.injectJs(Consts.POLYFILL);
+	if (!await this.property(HAS_INJECTED)) {
+		const injected = await this.injectJs(PROMISE_POLYFILL);
 
 		if (!injected) {
 			throw new Error(
-				`${Consts.ERROR_PREFIX}: Cannot find moudle "${Consts.POLYFILL}".` +
-				'\nMaybe this is a bug. Please add an issue to the project.'
+				`${ERROR_PREFIX}: Cannot find moudle "${PROMISE_POLYFILL}".` +
+				'\nMaybe this is a bug. Please create an issue to the project.'
 			);
 		}
 
-		this.property(Consts.HAS_INJECTED, injected);
+		this.property(HAS_INJECTED, injected);
 	}
 
 	if (delay) {
 		if (delay > timeout) {
 			throw new Error(
-				`${Consts.ERROR_PREFIX}: Option "timeout" MUST larger than "delay".`
+				`${ERROR_PREFIX}: Option "timeout" MUST larger than "delay".`
 			);
 		}
 		await execDelay(delay);
@@ -50,19 +49,36 @@ Page.prototype.evaluatePromise = async function (src, options = {}) {
 
 	const finalCode = `
 		function () {
-			return (${code})(${argsString}).then(function (result) {
-				window.callPhantom({
-					${Consts.CALL_PHANTOM}: result,
-				});
-			});
+			var promise = (${code})(${argsString});
+			if (typeof promise.then !== 'function') {
+				promise = Promise.resolve(promise);
+			}
+			return promise
+				.then(function (result) {
+					window.callPhantom({
+						${CALL_PHANTOM_FULFILLED}: result,
+					});
+				})
+				.catch(function (err) {
+					window.callPhantom({
+						${CALL_PHANTOM_REJECTED}: err.message || err,
+					});
+				})
+			;
 		}
 	`;
 
-	const execMain = new Promise((resolve) => {
+	const execMain = new Promise((resolve, reject) => {
 		const done = (data) => {
-			if (data && data[Consts.CALL_PHANTOM]) {
-				resolve(data[Consts.CALL_PHANTOM]);
-				this.off('onCallback', done);
+			this.off('onCallback', done);
+
+			if (!data) { return; }
+
+			if (data[CALL_PHANTOM_FULFILLED]) {
+				resolve(data[CALL_PHANTOM_FULFILLED]);
+			}
+			else if (data[CALL_PHANTOM_REJECTED]) {
+				reject(data[CALL_PHANTOM_REJECTED]);
 			}
 		};
 		this.on('onCallback', done);
